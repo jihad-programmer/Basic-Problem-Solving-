e6e3564259cf2426581ef3deac8ae67369bb57f5
jihad-programmer <mdjaminur2300@gmail.com>
2024-05-31 01:37:52 -0700

    solve done

---
+++ name.js
@@ -0,0 +1,94 @@
+//1 no.
+
+let carName = "Volvo";
+
+console.log(carName)
+
+//2 no.
+
+// let firstName = "John" , lastName = "Doe" , age = 35;
+
+// console.log(firstName)
+// console.log(lastName)
+// console.log(age)
+
+//3 no.
+
+// x = 10;
+// y = 5;
+// console.log(x *= y)     
+
+// 4 no.
+
+// let length = 16; //Number
+// let lastName = "Johnson"; // String
+
+
+// const x = {
+//     firstName: "John",  
+//     lastName: "Doe"
+//   };    // Object 
+  
+
+// 5 no.
+
+
+//  function myFunction() {
+//   alert("Hello World!");
+// }
+
+// myFunction();
+
+// 6 no.
+
+//  const person = {
+//     name: "John" ,
+//     age: 50
+//  } 
+//  alert(person.name + " is " + person.age)
+
+
+// 7 no. in HTML
+
+
+
+// 8 no.
+
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// alert(cars.length)
+    
+
+// const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+// Brand [0] = "Ford"
+// console.log(Brand)
+
+
+
+// 9 no.
+
+// let random = Math.random()
+// console.log(random)
+
+
+// let maximum = Math.max(10,11,12,13,14,15,16,17,18,19,20)
+// console.log(maximum)
+
+
+
+// let sqr = Math.sqrt(9)
+// console.log(sqr)
+
+
+// 10 no.
+
+// x = 10;
+// y = 5;
+
+// alert (x > y)
+
+
+
+// var age = n;
+// var voteable = (18 > age) ? "Too young" : "Old enough";
+
+// alert(voteable);
